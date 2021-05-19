42fa872a43949f0ac290f23e2e5ac6741c9f7c37
William Wong <william1104@gmail.com>
2021-05-19 13:51:32 +0800

    enhance jmh test cases to cover multiple scenarios

--- kotlin/src/jmh/java/hin/sudoku/solver/SolverBenchmark.java
+++ kotlin/src/jmh/java/hin/sudoku/solver/SolverBenchmark.java
@@ -1,12 +1,11 @@
 package hin.sudoku.solver;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
+import will.sudoku.solver.Board;
 import will.sudoku.solver.BoardReader;
 import will.sudoku.solver.Solver;
+import will.sudoku.solver.SolverTest;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,38 +13,21 @@ public class SolverBenchmark {
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void simplePuzzle(Blackhole bh) {
+    public void solvePuzzle(Blackhole bh, BenchmarkState state) {
         Solver solver = new Solver();
-        bh.consume(solver.solve(BoardReader.readBoard(
-                ".4.!3.8!1.." +
-                        "21.!.65!..." +
-                        "6..!...!.7." +
-                        "---!---!---" +
-                        "9.3!.46!781" +
-                        "1.4!829!5.6" +
-                        "8.5!...!.2." +
-                        "---!---!---" +
-                        "4..!...!6.3" +
-                        "...!6.2!.47" +
-                        ".8.!.3.!...")));
+        bh.consume(solver.solve(state.board));
     }
 
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void hardPuzzle(Blackhole bh) {
-        Solver solver = new Solver();
-        bh.consume(solver.solve(BoardReader.readBoard(
-                "7..!.4.!2.." +
-                        "...!52.!..6" +
-                        "...!...!5.." +
-                        "---!---!---" +
-                        ".7.!...!96." +
-                        ".6.!...!.8." +
-                        "425!...!..." +
-                        "---!---!---" +
-                        "...!..9!.31" +
-                        "..4!..7!..." +
-                        "1..!6..!...")));
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+        @Param({"g1", "g2", "g3", "g4"})
+        public String boardName;
+
+        public Board board;
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            board = BoardReader.readBoard(SolverTest.class.getResourceAsStream("/solver/www.sudokuweb.org/" + boardName + ".question"));
+        }
     }
 }
